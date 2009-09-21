5a90f03072715d1d872d791b9dcc704f86eb2b0a
Rob Sanheim <rsanheim@gmail.com>
2009-09-21 00:58:37 -0400

    override attrs

--- roles/web.rb
+++ roles/web.rb
@@ -1,7 +1,7 @@
 name "web"
 recipes "passenger_apache2"
 
-default_attributes "passenger" => { "version" => "2.2.5" }
+override_attributes "passenger" => { "version" => "2.2.5" }
 
 # web_app "some_rails_app" do
 #   docroot "/srv/some-app/current/public"
