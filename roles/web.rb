17e8e76bf93fc81656b435fe1fd53eb784ba2eb9
Rob Sanheim <rsanheim@gmail.com>
2009-09-21 00:57:34 -0400

    set passenger versoin

--- roles/web.rb
+++ roles/web.rb
@@ -1,6 +1,8 @@
 name "web"
 recipes "passenger_apache2"
 
+default_attributes "passenger" => { "version" => "2.2.5" }
+
 # web_app "some_rails_app" do
 #   docroot "/srv/some-app/current/public"
 #   template "some_rails_app.conf.erb"
