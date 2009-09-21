911ea7f6621ff574f4a93ee10c398f687986c5af
Rob Sanheim <rsanheim@gmail.com>
2009-09-21 00:02:26 -0400

    bootstrap

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,19 +3,58 @@ set :repository,  "set your repository location here"
 
 set :scm, :subversion
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, "ec2-174-129-180-22.compute-1.amazonaws.com"
+role :app, "ec2-174-129-180-22.compute-1.amazonaws.com"
+# role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+# role :db,  "your slave db-server here"
 
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
+namespace :deploy do
+  task :start do
+  end
+  task :stop do
+  end
+  task :restart do
+  end
+end
 
-# namespace :deploy do
-#   task :start {}
-#   task :stop {}
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :chef do
+  task :web_json, :roles => :web do
+    puts 'hi'
+    run "whoami"
+  end
+  
+  task :install_chef do
+    sudo "/usr/bin/gem1.8 install chef ohai rake --source http://gems.opscode.com --source http://gems.rubyforge.org --no-ri --no-rdoc"
+  end
+
+  desc "Install Ruby from Ubuntu sources in order to allow us to install Chef"
+  task :install_ruby do
+    sudo "apt-get install ruby ruby1.8-dev libopenssl-ruby1.8 rdoc ri irb build-essential wget ssl-cert -y"
+  end
+
+  task :install_rubygems do
+    set :user, "root"
+    rubygems = "rubygems-1.3.5"
+    run <<-EOC
+if [ ! -e /usr/bin/gem ]; then
+  wget http://rubyforge.org/frs/download.php/60718/#{rubygems}.tgz
+  && tar xzf #{rubygems}.tgz
+  && cd #{rubygems}
+  && /usr/bin/ruby setup.rb
+  && ln -s /usr/bin/gem1.8 /usr/bin/gem;
+else
+  echo "Rubygems already installed at /usr/bin/gem";
+fi
+EOC
+  end
+  
+  task :bootstrap do
+    top.deploy.setup
+    top.deploy.default
+    install_ruby
+    install_rubygems
+    install_chef
+    # solo
+  end
+  
+end
