a6636ca68d15eb47e16df33f29751982020477e6
Rob Sanheim <rsanheim@gmail.com>
2009-09-21 00:32:36 -0400

    more paths and start taking notes

--- config/solo.rb
+++ config/solo.rb
@@ -4,7 +4,11 @@
 
 log_level          :debug
 log_location       STDOUT
+
+cookbook_path      [ "/srv/chef/site-cookbooks", "/srv/chef/cookbooks" ]
+file_store_path    "/srv/chef/file_store"
 file_cache_path    "/srv/chef/cookbooks"
-role_path "/srv/chef/roles"
+role_path          "/srv/chef/roles"
+
 ssl_verify_mode    :verify_none
 Chef::Log::Formatter.show_time = true
