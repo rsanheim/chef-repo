8c715bd756384aa9bebbcf5c42cb37b488c49602
Rob Sanheim <rsanheim@gmail.com>
2009-09-21 00:21:43 -0400

    debug

--- config/solo.rb
+++ config/solo.rb
@@ -2,7 +2,7 @@
 # Chef Solo Config File
 #
 
-log_level          :info
+log_level          :debug
 log_location       STDOUT
 file_cache_path    "/srv/chef/cookbooks"
 ssl_verify_mode    :verify_none
