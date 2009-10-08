b334007c2252cc8ee1bf5b9f4cfb119ae6f42f96
Rob Sanheim <rsanheim@gmail.com>
2009-10-07 23:35:02 -0400

    add mod rails recipe

--- roles/web.rb
+++ roles/web.rb
@@ -1,4 +1,4 @@
 name "web"
-recipes "passenger_apache2", "braincron"
+recipes "apache2", "passenger_apache2", "passenger_apache2::mod_rails", "braincron"
 
 override_attributes "passenger" => { "version" => "2.2.5" }
