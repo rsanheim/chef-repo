819f7d5a6b4e9cbafb3d7b50bd8e82e2aabaf2fa
Rob Sanheim <rsanheim@gmail.com>
2009-10-07 23:13:59 -0400

    add braincron conf for passenger

--- site-cookbooks/braincron/recipes/default.rb
+++ site-cookbooks/braincron/recipes/default.rb
@@ -1,5 +1,29 @@
+directory "/data" do
+  owner node[:user]
+  mode 0755
+end
+
+node[:apps].each do |app|
+
+  cap_directories = [
+    "/srv/#{app}/shared",
+    "/srv/#{app}/shared/config",
+    "/srv/#{app}/shared/system",
+    "/srv/#{app}/releases" 
+  ]
+
+  cap_directories.each do |dir|
+    directory dir do
+      owner node[:user]
+      mode 0755
+      recursive true
+    end
+  end
+
+end
+
 web_app "braincron" do
-  docroot "/srv/braincron/public"
+  docroot "/srv/braincron/current/public"
   server_name "braincron.#{node[:domain]}"
   server_aliases [ "braincron", node[:hostname] ]
   rails_env "production"
