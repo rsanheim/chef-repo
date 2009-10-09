eebffa6da5d213c1b5a37a03f3a0b89441673fe5
Rob Sanheim <rsanheim@gmail.com>
2009-10-09 00:36:51 -0400

    okay, deploy is built in now

--- site-cookbooks/braincron/recipes/deploy.rb
+++ site-cookbooks/braincron/recipes/deploy.rb
@@ -1,16 +1,12 @@
-require File.join(File.dirname(__FILE__), *%w[.. .. .. vendor chef-deploy lib chef-deploy])
-
 deploy "#{node[:app_root]}/braincron" do
-   repo "git://github.com/rsanheim/braincron.git"
-   branch "HEAD"
-   user "deploy"
-   role "web"
-   enable_submodules true
-   migrate true
-   migration_command "rake db:migrate"
-   environment "production"
-   shallow_clone true
-   revision "HEAD"
-   restart_command "touch tmp/restart.txt" # "monit restart all -g foo", etc.
-   action :deploy # or :rollback
- end
+  repo "git://github.com/rsanheim/braincron.git"
+  revision "HEAD" # or "HEAD" or "TAG_for_1.0" or (subversion) "1234"
+  user "deploy"
+  enable_submodules true
+  migrate true
+  migration_command "rake db:migrate"
+  environment "RAILS_ENV" => "production"
+  shallow_clone true
+  restart_command "touch tmp/restart.txt"
+  action :deploy # or :rollback
+end
