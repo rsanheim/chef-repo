b3d6de83a9bfa3d00a2b25d451b3936964b5ac38
Rob Sanheim <rsanheim@gmail.com>
2009-10-09 01:06:04 -0400

    setup db

--- roles/web.rb
+++ roles/web.rb
@@ -1,4 +1,5 @@
 name "web"
 recipes "braincron", "braincron::deploy"
 
+default_attributes "app_name" => "braincron"
 override_attributes "passenger" => { "version" => "2.2.5" }
--- site-cookbooks/braincron/recipes/deploy.rb
+++ site-cookbooks/braincron/recipes/deploy.rb
@@ -1,4 +1,18 @@
-deploy "#{node[:app_root]}/braincron" do
+node[:gems].each do |gem|
+  gem_package gem do
+    action :install
+  end
+end
+
+template "#{node[:app_root]}/#{node[:app_name]}/shared/config/database.yml" do
+  source "database.yml.erb"
+  owner "deploy"
+  group "deploy"
+  variables :app_name => node[:app_name]
+  mode "0664"
+end
+
+deploy "#{node[:app_root]}/#{node[:app_name]}" do
   repo "git://github.com/rsanheim/braincron.git"
   revision "HEAD" # or "HEAD" or "TAG_for_1.0" or (subversion) "1234"
   user "deploy"
