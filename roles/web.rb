a222e08f623f2b305308dc9347ed2d0b3cfde846
Rob Sanheim <rsanheim@gmail.com>
2009-10-10 01:15:19 -0600

    passenger

--- roles/web.rb
+++ roles/web.rb
@@ -2,4 +2,4 @@ name "web"
 recipes "braincron", "braincron::deploy"
 
 default_attributes "app_name" => "braincron"
-override_attributes "passenger" => { "version" => "2.2.4" }
+override_attributes "passenger" => { "version" => "2.2.5" }
