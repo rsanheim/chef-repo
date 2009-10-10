5501cb54cd6df12580023958da97a2d5a127a6c7
Rob Sanheim <rsanheim@gmail.com>
2009-10-10 09:50:22 -0600

    assign the var

--- site-cookbooks/braincron/recipes/cron.rb
+++ site-cookbooks/braincron/recipes/cron.rb
@@ -1,4 +1,5 @@
-current_path "#{node[:app_root]}/#{node[:app_name]}/current"
+current_path = "#{node[:app_root]}/#{node[:app_name]}/current"
+
 cron "run reminders every minute" do
   user "deploy"
   path "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin"
